ac8f9bbeac1a03e090f30511d5eb15340963126a
supersand24 <supersand24@gmail.com>
2019-12-22 17:45:33 -0500

    Added checkIfValidMove() for King.java

--- src/ArtifactScripts/King.java
+++ src/ArtifactScripts/King.java
@@ -1,5 +1,7 @@
 package ArtifactScripts;
 
+import BoardScripts.Board;
+
 public class King extends Artifact {
 
     public King() {
@@ -7,6 +9,18 @@ public class King extends Artifact {
         yPos = 8;
         idCurrent = 'K';
         idOther = 'k';
+    }
+
+    public boolean checkIfValidMove(int xTarget, int yTarget, Board board) {
+        boolean canMove = false;
+        if (Math.abs(xTarget - xPos) <= 1 || Math.abs(yTarget - yPos) <= 1) {
+            if (checkVertical(xTarget, yTarget, board) || checkHorizontal(xTarget, yTarget, board) || checkDiagonally(xTarget, yTarget, board)) {
+                canMove = true;
+            } else {
+                System.out.println("Invalid Move, you can only move horizontally, vertically, or diagonally 1 space with a King.");
+            }
+        }
+        return canMove;
     }
 
 }
