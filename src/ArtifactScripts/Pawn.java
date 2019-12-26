778dc41d2ecbad7d5ef6f1971d30c8864a4a0aa0
supersand24 <supersand24@gmail.com>
2019-12-26 18:52:53 -0500

    Fixed error with attacking diagonally Pawn.java

--- src/ArtifactScripts/Pawn.java
+++ src/ArtifactScripts/Pawn.java
@@ -27,8 +27,10 @@ public class Pawn extends Artifact{
         yTarget += 1;
         //If target is on the same column
         if (xTarget == xPos) {
+            System.out.println("Target is on same column");
             //If target is two spaces away and on same column
             if (Math.abs(yTarget - yPos) == 2) {
+                System.out.println("Distance is 2");
                 //Check if the Pawn has moved before
                 if (moved) {
                     System.out.println("Invalid Move, you can only move two spaces only on the first turn of that Pawn.");
@@ -51,6 +53,7 @@ public class Pawn extends Artifact{
                 }
             //If target is one space away and on same column
             } else if (Math.abs(yTarget - yPos) == 1) {
+                System.out.println("Distance is 1");
                 if (board.isPlaceFull(new int[]{xTarget,yTarget})) {
                     System.out.println("Invalid Move, there is a chess piece in that spot, and a Pawn may not capture whats directly in front of it.");
                 } else {
@@ -60,7 +63,8 @@ public class Pawn extends Artifact{
                 System.out.println("Invalid Move, you can only move 1 or 2(on the Pawn's first turn) spaces forwards.");
             }
         //If target is on the next column over
-        } else if (Math.abs(xTarget - xPos) == 1) {
+        } else if (Math.abs(xTarget - xPos) == 1 || Math.abs(yTarget - yPos) == 1) {
+            System.out.println("Attacking");
             if ((yTarget - yPos) > 0) {
                 if (team == 0) {
                     canMove = true;
